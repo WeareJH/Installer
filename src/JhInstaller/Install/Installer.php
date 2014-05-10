4d8b83bd034c82e175aa15a21ceddb864d9c215d
Aydin Hassan <aydin@hotmail.co.uk>
2014-05-10 12:34:01 +0100

    Display message if no modules require installing

--- src/JhInstaller/Install/Installer.php
+++ src/JhInstaller/Install/Installer.php
@@ -40,55 +40,65 @@ class Installer implements ServiceLocatorAwareInterface
      */
     public function installModules(AdapterInterface $console)
     {
-        foreach ($this->moduleManager->getLoadedModules(false) as $module) {
-            if ($module instanceof Installable) {
+
+        //create array of installable modules
+        $installableModules = array_filter($this->moduleManager->getLoadedModules(false), function ($module) {
+            return $module instanceof Installable;
+        });
+
+        //return early if no modules require installing
+        if (!count($installableModules)) {
+            $console->writeLine("No Modules Require installing", Color::MAGENTA);
+            return;
+        }
+
+        foreach ($installableModules as $module) {
+            $console->writeLine(
+                sprintf("Attempting to install: %s", substr(get_class($module), 0, -7)),
+                Color::GREEN
+            );
+
+            $installerService = $module->getInstallService();
+
+            if (!$this->serviceLocator->has($installerService)) {
                 $console->writeLine(
-                    sprintf("Attempting to install: %s", substr(get_class($module), 0, -7)),
-                    Color::GREEN
+                    sprintf(
+                        "Could Not find Installer Service: %s For Module %s. Skipping.",
+                        $installerService,
+                        substr(get_class($module), 0, -7)
+                    ),
+                    Color::RED
                 );
+                continue;
+            }
 
-                $installerService = $module->getInstallService();
+            $moduleInstaller = $this->serviceLocator->get($installerService);
 
-                if (!$this->serviceLocator->has($installerService)) {
-                    $console->writeLine(
-                        sprintf(
-                            "Could Not find Installer Service: %s For Module %s. Skipping.",
-                            $installerService,
-                            substr(get_class($module), 0, -7)
-                        ),
-                        Color::RED
-                    );
-                    continue;
-                }
+            if (!$moduleInstaller instanceof InstallerInterface) {
+                $console->writeLine(
+                    sprintf(
+                        "Installer Service Must Implement: %s For Module %s. Skipping.",
+                        'JhInstaller\Install\InstallerInterface',
+                        substr(get_class($module), 0, -7)
+                    ),
+                    Color::RED
+                );
+                continue;
+            }
 
-                $moduleInstaller = $this->serviceLocator->get($installerService);
+            //run module installer
+            try {
+                $moduleInstaller->install($console);
+            } catch (InstallException $e) {
+                //catch fatal errors, so it doesn't continue
+            }
 
-                if (!$moduleInstaller instanceof InstallerInterface) {
-                    $console->writeLine(
-                        sprintf(
-                            "Installer Service Must Implement: %s For Module %s. Skipping.",
-                            'JhInstaller\Install\InstallerInterface',
-                            substr(get_class($module), 0, -7)
-                        ),
-                        Color::RED
-                    );
-                    continue;
-                }
+            //if there were any errors
+            if ($moduleInstaller->getErrors()) {
+                $console->writeLine("Errors Occured: ", Color::RED);
 
-                //run module installer
-                try {
-                    $moduleInstaller->install($console);
-                } catch (InstallException $e) {
-                    //catch fatal errors, so it doesn't continue
-                }
-
-                //if there were any errors
-                if ($moduleInstaller->getErrors()) {
-                    $console->writeLine("Errors Occured: ", Color::RED);
-
-                    foreach ($moduleInstaller->getErrors() as $error) {
-                        $console->writeLine($error, Color::RED);
-                    }
+                foreach ($moduleInstaller->getErrors() as $error) {
+                    $console->writeLine($error, Color::RED);
                 }
             }
         }
